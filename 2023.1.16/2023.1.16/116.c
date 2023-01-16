c77cbf6dfbea598239c92e6db98e5d61086ce2de
Heejin <1719223024@qq.com>
2023-01-16 20:54:23 +0800

    20230116

---
+++ 2023.1.16/2023.1.16/116.c
@@ -0,0 +1,33 @@
+//#include<stdio.h>
+//int main()
+//{
+//	char password[20];
+//	printf("请输入密码:");
+//	scanf_s("%s", password);
+//	printf("确定吗(请输入Y/N)");
+//	/*getchar();
+//	int ch = getchar();
+//	if (ch == 'Y')
+//	{
+//	printf("确认成功");
+//	}
+//	else
+//	{
+//	printf("确认失败");
+//	}*/
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (; i < 3; i++)
+	{
+		for (; j < 3; j++)
+		{
+			printf("haha\n");
+		}
+    }
+	return 0;
+}
