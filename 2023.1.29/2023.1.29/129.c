ed680d0f82826894ea089a563e20c2be70d3334f
Heejin <1719223024@qq.com>
2023-01-29 20:32:25 +0800

    20230129

---
+++ 2023.1.29/2023.1.29/129.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*int main()
+{
+	printf("Practice makes perfect!");
+	return 0;
+}*/
+/*int main()
+{
+	printf("v   v\n");
+	printf(" v v\n");
+	printf("  v\n");
+	return 0;
+}*/
+/*int main()
+//{
+	short a=0;
+	int b=0;
+	long c=0;
+	long long d=0;
+	printf("The size of short is %d bytes.\n", sizeof(a));
+	printf("The size of int is %d bytes.\n", sizeof(b));
+	printf("The size of long is %d bytes.\n", sizeof(c));
+	printf("The size of long long is %d bytes.\n", sizeof(d));
+	return 0;
+}*/
+int main()
+{
+	printf("     **\n");
+	printf("     **\n");
+	printf("************\n");
+	printf("************\n");
+	printf("    *  *\n");
+	printf("    *  *\n");
+	return 0;
+}
