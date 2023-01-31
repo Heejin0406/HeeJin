7e4887febe1fa5e74731ba6ac0584613aaa5511c
Heejin <1719223024@qq.com>
2023-01-31 23:17:54 +0800

    20230131

---
+++ 2023.1.31/2023.1.31/131.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+/*{
+int a = 1234;
+printf("%#o %#X\n",a,a);
+return 0;
+}*/
+/*{
+int a = 0xABCDEF;
+printf("%15d\n",a);
+return 0;
+}*/
+/*{
+int a=printf("Hello world!");
+printf("\n");
+printf("%d\n", a);
+return 0;
+}*/
+{
+	printf("\n%d\n", printf("Hello world!"));
+	return 0;
+}
