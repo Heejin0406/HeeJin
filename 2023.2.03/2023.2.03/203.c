e194122777dd601cda994c48d1ee511b07e44877
Heejin <1719223024@qq.com>
2023-02-03 17:27:47 +0800

    20230203

---
+++ 2023.2.03/2023.2.03/203.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+/*int main()
+{
+	int a, b, c;
+	scanf("%4d%2d%2d",&a,&b,&c);
+	printf("year=%4d\nmonth=%02d\ndate=%02d\n",a,b,c);
+	return 0;
+}*/
+/*int main()
+{
+	int m, n;
+	scanf("a=%d,b=%d", &m, &n);
+	printf("a=%d,b=%d", n, m);
+	return 0;
+}*/
+/*int main()
+{
+	char n;
+	scanf("%c", &n);
+	printf("%d", n);
+	return 0;
+}*/
+int main()
+{
+	int a = 40;
+	int c=212;
+	int n=(-8 + 22)*a - 10 + c/2;
+	printf("%d", n);
+	return 0;
+}
