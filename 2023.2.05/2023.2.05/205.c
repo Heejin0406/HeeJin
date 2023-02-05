9bb70e36cc6a64a664d4b1440e3c805c58f606d5
Heejin <1719223024@qq.com>
2023-02-05 18:09:24 +0800

    20230205

---
+++ 2023.2.05/2023.2.05/205.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+/*int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	int c = a + b;
+	printf("%d\n", c % 100);
+	return 0;
+}*/
+/*int main()
+{
+	float a;
+	scanf("%f", &a);
+	int b=a;
+	printf("%d\n", b % 10);
+	return 0;
+}*/
+/*int main()
+{
+	int age = 0;
+	scanf("%d", &age);
+	long int s = age * 31560000;
+	printf("%ld\n", s);
+	return 0;
+}*/
+int main()
+{
+	int age = 0;
+	scanf("%d",&age);
+	int s = age*3156;
+	printf("%d0000\n", s);
+	return 0;
+}
