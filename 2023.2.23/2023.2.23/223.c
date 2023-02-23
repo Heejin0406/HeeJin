bf365841f59e1632b70ae2c699cba24dbcad2b47
Heejin <1719223024@qq.com>
2023-02-23 13:07:18 +0800

    20230223

---
+++ 2023.2.23/2023.2.23/223.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	char n = 0;
+	int A = 0;
+	int B = 0;
+	while (scanf("%c", &n) != EOF)
+	{
+		if (n == '0')
+		{
+			break;
+		}
+		else if (n == 'A')
+		{
+			A++;
+		}
+		else
+		{
+			B++;
+		}
+	}
+	if (A > B)
+	{
+		printf("A\n");
+	}
+	else if (A < B)
+	{
+		printf("B\n");
+	}
+	else
+	{
+		printf("E\n");
+	}
+	return 0;
+}
