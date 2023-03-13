559e0c40fc727eb41190cad47ecb6320c7f9a23d
Heejin <1719223024@qq.com>
2023-03-13 23:03:44 +0800

    20230313

---
+++ 2023.3.13/2023.3.13/313.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int n;
+	while (scanf("%d", &n) != EOF)
+	{
+		for (int i = 0; i < n-1; i++)
+		{
+			printf("* ");
+			for (int j = 1; j < i +1; j++)
+			{
+				if (j == i )
+				{
+					printf("* ");
+				}
+				else
+				{
+					printf("  ");
+				}
+			}
+			printf("\n");
+		}
+		for (int k = 0; k < n; k++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+int main()
+{
+	double sum = 0.0;
+	for (int n = 0; n < 5; n++)
+	{
+		double a;
+		scanf("%lf", &a);
+		sum = sum + a;
+	}
+	double avg = sum / 5;
+	printf("%.2lf", avg);
+	return 0;
+}
