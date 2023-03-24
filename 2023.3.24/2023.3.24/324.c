1f70e910e00362ad1fdab212f87827c0ecc372fc
Heejin <1719223024@qq.com>
2023-03-24 23:24:08 +0800

    20230324

---
+++ 2023.3.24/2023.3.24/324.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int n;
+	while (scanf("%d", &n) != EOF)
+	{
+		if (n == 200)
+		{
+			printf("OK\n");
+		}
+		else if (n == 202)
+		{
+			printf("Accepted\n");
+		}
+		else if (n == 400)
+		{
+			printf("Bad Request\n");
+		}
+		else if (n == 403)
+		{
+			printf("Forbidden\n");
+		}
+		else if (n == 404)
+		{
+			printf("Not Found\n");
+		}
+		else if (n == 500)
+		{
+			printf("Internal Server Error\n");
+		}
+		else if (n == 502)
+		{
+			printf("Bad Gateway\n");
+		}
+	}
+	return 0;
+}
